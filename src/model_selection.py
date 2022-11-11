4522b2ebcd84522c4d5bee8f95cbc5ac028e40d8
dav1ddq0 <dquesada969x@gmail.com>
2022-11-11 14:38:45 -0500

    agregado sort dataframe mean test score

--- src/model_selection.py
+++ src/model_selection.py
@@ -26,6 +26,7 @@ import matplotlib.pyplot as plt
 
 from tools import remove_split_scores
 
+
 def select_model(series_outls: pd.DataFrame, class_vector: list[int]):
     '''
         Train and evaluate several Machine Learning supervised methods to find the one
@@ -46,21 +47,24 @@ def select_model(series_outls: pd.DataFrame, class_vector: list[int]):
     '''
 
     scaler = StandardScaler()
-    series_outls_scaled = pd.DataFrame(scaler.fit_transform(series_outls), columns=series_outls.columns)
+    series_outls_scaled = pd.DataFrame(scaler.fit_transform(
+        series_outls), columns=series_outls.columns)
 
-    X_train_scaled, X_test_scaled, y_train_scaled, y_test_scaled = train_test_split(series_outls_scaled, class_vector, train_size=0.6)
-    X_train, X_test, y_train, y_test = train_test_split(series_outls, class_vector, train_size=0.6)
+    X_train_scaled, X_test_scaled, y_train_scaled, y_test_scaled = train_test_split(
+        series_outls_scaled, class_vector, train_size=0.6)
+    X_train, X_test, y_train, y_test = train_test_split(
+        series_outls, class_vector, train_size=0.6)
 
     knn_param_grid = [
-        { 
-            'n_neighbors' : [3, 5, 7, 9, 11],
-            'weights' : ['uniform', 'distance'],
-            'algorithm': ['brute'] 
+        {
+            'n_neighbors': [3, 5, 7, 9, 11],
+            'weights': ['uniform', 'distance'],
+            'algorithm': ['brute']
         },
 
         {
-            'n_neighbors' : [3, 5, 7, 9, 11],
-            'weights' : ['uniform', 'distance'],
+            'n_neighbors': [3, 5, 7, 9, 11],
+            'weights': ['uniform', 'distance'],
             'algorithm': ['kd_tree', 'ball_tree'],
             'leaf_size': [20, 30, 40, 50]
         }
@@ -73,17 +77,17 @@ def select_model(series_outls: pd.DataFrame, class_vector: list[int]):
             'max_depth': [3, 4, 5, 6, None],
             'max_features': ['sqrt', 'log2', None]
         },
-    ] 
+    ]
 
     rdf_param_grid = [
         {
-            #'n_estimators': [100, 120, 140, 160, 180],
+            # 'n_estimators': [100, 120, 140, 160, 180],
             'n_estimators': [100, 120, 150, 240],
-            #'criterion': ['gini', 'entropy'],
+            # 'criterion': ['gini', 'entropy'],
             'criterion': ['entropy', 'gini'],
-            #'max_depth': [3, 4, 5, 6],
+            # 'max_depth': [3, 4, 5, 6],
             'max_depth': [20],
-            #'max_features': ['sqrt', 'log2', None]
+            # 'max_features': ['sqrt', 'log2', None]
             'max_features': ['log2', 'sqrt']
         }
     ]
@@ -91,12 +95,12 @@ def select_model(series_outls: pd.DataFrame, class_vector: list[int]):
     logreg_param_grid = [
         {
             'penalty': ['l2'],
-            #'tol': [1e-3, 1e-4, 1e-5, 1e-6],
-           'tol': [1e-3, 1e-5],
-            ##'C': [1, 10, 100, 1000],
+            # 'tol': [1e-3, 1e-4, 1e-5, 1e-6],
+            'tol': [1e-3, 1e-5],
+            # 'C': [1, 10, 100, 1000],
             'C': [1, 100],
             'solver': ['lbfgs'],
-            #'max_iter': [100, 500, 1000]
+            # 'max_iter': [100, 500, 1000]
             'max_iter': [100, 500]
         },
 
@@ -118,13 +122,13 @@ def select_model(series_outls: pd.DataFrame, class_vector: list[int]):
         # },
 
         {
-            #'C': [1, 10, 100, 1000],
+            # 'C': [1, 10, 100, 1000],
             'C': [1, 10, 100, 1000],
-            #'kernel': ['poly', 'rbf', 'sigmoid'],
-            'kernel': ['rbf'],
-            #'kernel': ['sigmoid'],
-            #'degree': [3, 4],
-            #'gamma': [0.01, 0.001, 0.0001],
+            # 'kernel': ['poly', 'rbf', 'sigmoid'],
+            # 'kernel': ['rbf'],
+            'kernel': ['sigmoid'],
+            # 'degree': [3, 4],
+            # 'gamma': [0.01, 0.001, 0.0001],
             'gamma': [0.01, 0.001, 0.0001],
             'probability': [True]
         }
@@ -137,25 +141,29 @@ def select_model(series_outls: pd.DataFrame, class_vector: list[int]):
     svm_clsf = SVC()
 
     clsfrs = [
-                #("KNN", knn_clsf, knn_param_grid),
-                #("Decision Tree", dt_clsf, dt_param_grid),
-                #("Random Forest", rdf_clsf, rdf_param_grid),
-                #("Log Regression", logreg_clsf, logreg_param_grid),
-                ("SVM", svm_clsf, svm_param_grid)
+        #("KNN", knn_clsf, knn_param_grid),
+        #("Decision Tree", dt_clsf, dt_param_grid),
+        #("Random Forest", rdf_clsf, rdf_param_grid),
+        #("Log Regression", logreg_clsf, logreg_param_grid),
+        ("SVM", svm_clsf, svm_param_grid)
     ]
 
     results = []
     for clsf_name, clsf, clsf_param_grid in clsfrs:
-        print(f"-------------------Running model {clsf_name}------------------------")
+        print(
+            f"-------------------Running model {clsf_name}------------------------")
         if clsf_name == "Log Regression":
-            clsf_gs_results = (clsf_name, train_with_cv(clsf, clsf_param_grid, X_train_scaled, y_train_scaled))         
+            clsf_gs_results = (clsf_name, train_with_cv(
+                clsf, clsf_param_grid, X_train_scaled, y_train_scaled))
 
         else:
-            clsf_gs_results = (clsf_name, train_with_cv(clsf, clsf_param_grid, X_train, y_train))         
+            clsf_gs_results = (clsf_name, train_with_cv(
+                clsf, clsf_param_grid, X_train, y_train))
 
         results.append(clsf_gs_results)
 
     return results
+
 
 def train_with_cv(clsf, param_grid, X_train: pd.DataFrame, y_train: list[int]):
     '''
@@ -179,10 +187,16 @@ def train_with_cv(clsf, param_grid, X_train: pd.DataFrame, y_train: list[int]):
     '''
 
     f1_scorer = make_scorer(f1_score)
-    cross_validator = RepeatedStratifiedKFold(n_splits=5, n_repeats=30, random_state=121)
-    hyp_estm_cv = GridSearchCV(estimator=clsf, param_grid=param_grid, scoring=f1_scorer, cv=cross_validator, n_jobs=-1)
+    cross_validator = RepeatedStratifiedKFold(
+        n_splits=5, n_repeats=30, random_state=121)
+    hyp_estm_cv = GridSearchCV(estimator=clsf, param_grid=param_grid,
+                               scoring=f1_scorer, cv=cross_validator, n_jobs=-1)
     hyp_estm_cv.fit(X_train, y_train)
 
     gsearch_results = pd.DataFrame(hyp_estm_cv.cv_results_)
+
+    gsearch_results = gsearch_results.sort_values(
+        by="mean_test_score",     ascending=False
+    )
 
     return remove_split_scores(gsearch_results)
