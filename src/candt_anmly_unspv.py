d975035fc3b21dacb7d73b2d95c5d4051cff49bd
dav1ddq0 <dquesada969x@gmail.com>
2022-11-21 10:53:21 -0500

    agregado tuneo de ocvsvm y dbscan

--- src/candt_anmly_unspv.py
+++ src/candt_anmly_unspv.py
@@ -25,31 +25,46 @@ def find_candidates_unspv(time_series: pd.DataFrame) -> tuple:
 
     values = time_series[["X Accel", "Y Accel", "Z Accel", "X Gyro", "Y Gyro", "Z Gyro"]].values
 
-    y_pred_dbscan = DBSCAN(eps=0.6, min_samples=30).fit_predict(values)
-    y_pred_optics = OPTICS(min_samples=3).fit_predict(values)
-    y_pred_ocsvm = OneClassSVM(kernel="rbf", gamma="scale").fit_predict(values)
+    #y_pred_dbscan = DBSCAN(eps=0.6, min_samples=30).fit_predict(values)
+    #y_pred_optics = OPTICS(min_samples=3).fit_predict(values)
+    #y_pred_ocsvm = OneClassSVM(kernel="rbf", gamma="scale").fit_predict(values)
 
+    y_pred_dbscan = DBSCAN(eps=0.99, min_samples=15).fit_predict(values)
     # Silhouette Metric
-    best_hparams = search_best_dbscan_hparams(values)
-    print(f"best eps dbscacn: {best_hparams['eps']}")
-    print(f"best min_samples: {best_hparams['min_samples']}")
+    # best_hparams_dbscan = search_best_dbscan_hparams(values)
+    best_hparams_ocsvm = search_bebst_ocsvm_hparams(values)
+    # print(f"best eps dbscacn: {best_hparams_dbscan['eps']}")
+    # print(f"best min_samples: {best_hparams_dbscan['min_samples']}")
+    #y_pred_dbscan = DBSCAN(eps=best_hparams_dbscan['eps'], min_samples=best_hparams_dbscan['min_samples']).fit_predict(values)
     score_dbscan = metrics.silhouette_score(values, y_pred_dbscan)
-    score_optics = metrics.silhouette_score(values, y_pred_optics)
-    score_ocsvm = metrics.silhouette_score(values, y_pred_ocsvm)
+    #score_optics = metrics.silhouette_score(values, y_pred_optics)
+    #score_ocsvm = metrics.silhouette_score(values, y_pred_ocsvm)
     print('Silhouette score DBSCAN: {}'.format(score_dbscan))
-    print('Silhouette score OPTICS: {}'.format(score_optics))
+    # print("Best eps: {}".format(best_hparams_dbscan['eps']))
+    # print("Best min_samples: {}".format(best_hparams_dbscan['min_samples']))
+
+    #y_pred_ocsvm = OneClassSVM(kernel="rbf", gamma=best_hparams_ocsvm['gamma']).fit_predict(values)
+    y_pred_ocsvm = OneClassSVM(kernel="rbf", gamma=best_hparams_ocsvm['gamma'], nu=best_hparams_ocsvm['nu']).fit_predict(values)
+    score_ocsvm = metrics.silhouette_score(values, y_pred_ocsvm)
     print('Silhouette score OneClassSVM: {}'.format(score_ocsvm))
+    print("Best gamma: {}".format(best_hparams_ocsvm['gamma']))
+    print("Best Nu: {}".format(best_hparams_ocsvm['nu']))
+    #print('Silhouette score OPTICS: {}'.format(score_optics))
+    #print('Silhouette score OneClassSVM: {}'.format(score_ocsvm))
 
-    return y_pred_dbscan, y_pred_optics, y_pred_ocsvm, score_dbscan, score_optics, score_ocsvm
-
+    #return y_pred_dbscan, y_pred_optics, y_pred_ocsvm, score_dbscan, score_optics, score_ocsvm
+    return y_pred_dbscan, y_pred_dbscan, y_pred_ocsvm, score_dbscan, score_dbscan, score_ocsvm 
 
 def search_best_dbscan_hparams(values):
-    epsilon_values = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6 ,0.7 , 0.8, 0.9, 0.10]
-    min_samples_values = [15, 30, 50, 70]
+    epsilon_values = [0.05, 0.1, 0.15, 0.20, 0.25, 0.30, 0.35, 0.40, 0.45, 0.5, 0.55, 0.60 , 0.65, 0.70, 0.75,  0.80, 0.85, 0.90, 0.95, 0.99]
+    min_samples_values = [15, 20, 25, 30, 25, 40,  45, 50, 55, 60, 65]
     best_hparams = {}
     for eps in epsilon_values:
         for min_samples in min_samples_values:
-            y_pred_dbscan =  DBSCAN(eps=epsilon_values, min_samples=min_samples_values).fit_predict(values)
+            y_pred_dbscan =  DBSCAN(eps=eps, min_samples=min_samples).fit_predict(values)
+            if len(y_pred_dbscan) > 1 and all(elem == y_pred_dbscan[0] for elem in y_pred_dbscan):
+                continue
+
             score_dbscan = metrics.silhouette_score(values, y_pred_dbscan)
             config =  {
                     "eps": eps,
@@ -60,6 +75,32 @@ def search_best_dbscan_hparams(values):
                best_hparams =  config if  score_dbscan > best_hparams['score'] else best_hparams
             else:
                 best_hparams = config
+    return best_hparams
+
+def search_bebst_ocsvm_hparams(values):
+    gammas_values =  ["scale", 0.00001, 0.0001, 0.001, 0.01, 0.1, 1, 10]
+    nu_values= [0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 0.99]
+    best_hparams = {}
+    for nu in nu_values:
+        for g in gammas_values:
+            y_pred_ocsvm = OneClassSVM(kernel="rbf", gamma=g, nu=nu).fit_predict(values)
+            if len(y_pred_ocsvm) > 1 and all(elem == y_pred_ocsvm[0] for elem in y_pred_ocsvm):
+                continue
+
+            score_dbscan = metrics.silhouette_score(values, y_pred_ocsvm)
+            config =  {
+                "gamma": g,
+                "nu": nu,
+                "score" : score_dbscan
+            }
+            if best_hparams:
+                best_hparams =  config if  score_dbscan > best_hparams['score'] else best_hparams
+            else:
+                best_hparams = config
+    return best_hparams
+
+def search_best_optics_hparams(values):
+    y_pred_optics = OPTICS(min_samples=3).fit_predict(values)    
 
 def search_best_ocsvm_hparams(values):
     # 0.001, 0.01, 0.1, 1, 10, 100 gammas 
--- src/main.py
+++ src/main.py
@@ -101,65 +101,65 @@ def main(args):
     print("Mean Silhouette score OCSVM: {}".format(mean_ocsvm_score))
     print("------------------------------------------------------------------------------------")
 
-    outliers_df.pop("g_zero")
-    outliers_df.pop("optics")
-    best_model_config = {}
-    for outl_method_name in outliers_df.keys():
-        output_csv_idx = 1
-        X, y = outliers_df[outl_method_name]
-        # if there is any outliers detected with outl_method_name
-        if len(X) > 10:
-            # distinct number of features to select for each method of feature selection
-            for n_features_to_select in [3, 6, 9]:
-                features_selected_sets = feature_selection(
-                    X, y, n_features_to_select)
+    # outliers_df.pop("g_zero")
+    # outliers_df.pop("optics")
+    # best_model_config = {}
+    # for outl_method_name in outliers_df.keys():
+    #     output_csv_idx = 1
+    #     X, y = outliers_df[outl_method_name]
+    #     # if there is any outliers detected with outl_method_name
+    #     if len(X) > 10:
+    #         # distinct number of features to select for each method of feature selection
+    #         for n_features_to_select in [3, 6, 9]:
+    #             features_selected_sets = feature_selection(
+    #                 X, y, n_features_to_select)
 
-                print(
-                    f"---------------- Features selected with outliers detected using {outl_method_name} ----------------\n")
+    #             print(
+    #                 f"---------------- Features selected with outliers detected using {outl_method_name} ----------------\n")
 
-                for selector_name, features_selected_set in features_selected_sets:
-                    f_df = load_feature_df_count()
-                    for feature in features_selected_set:
-                        f_df[feature] = [f_df[feature].values[0] + 1]
-                    update_csv_feature_df_count(f_df)
-                    print(f"Features selected with selector {selector_name}")
-                    print(f"{features_selected_set}\n")
-                    df_sel_feats: pd.DataFrame = pd.DataFrame(
-                        X[features_selected_set])
+    #             for selector_name, features_selected_set in features_selected_sets:
+    #                 f_df = load_feature_df_count()
+    #                 for feature in features_selected_set:
+    #                     f_df[feature] = [f_df[feature].values[0] + 1]
+    #                 update_csv_feature_df_count(f_df)
+    #                 print(f"Features selected with selector {selector_name}")
+    #                 print(f"{features_selected_set}\n")
+    #                 df_sel_feats: pd.DataFrame = pd.DataFrame(
+    #                     X[features_selected_set])
 
-                    ms_results = select_model(df_sel_feats, y, model_input_name)
-                    #confusion_m
-                    for model_selected_name, result, prec, recall,  acc, f1, confusion_m in ms_results:
-                        print(f"----- Results with {model_selected_name} model and {outl_method_name} outlier mehotd-----")
-                        print(f"{result}\n\n")
-                        matrix_id = generate_unique_id_matrix(model_selected_name)
+    #                 ms_results = select_model(df_sel_feats, y, model_input_name)
+    #                 #confusion_m
+    #                 for model_selected_name, result, prec, recall,  acc, f1, confusion_m in ms_results:
+    #                     print(f"----- Results with {model_selected_name} model and {outl_method_name} outlier mehotd-----")
+    #                     print(f"{result}\n\n")
+    #                     matrix_id = generate_unique_id_matrix(model_selected_name)
 
-                        config = {
-                            "outliers_method": outl_method_name,
-                            "feature_selector": selector_name,
-                            "features_selected_set": features_selected_set,
-                            "model": model_selected_name,
-                            "best-config": result["params"].values[:1][0],
-                            "f1_score_train": result["mean_test_score"].values[:1][0],
-                            "f1_score_test": f1,
-                            "precision_test": prec,
-                            "recall_test": recall,
-                            "accuracy_test": acc,
-                            "confusion_matrix_path": f"{images_dir}/{matrix_id}.png"         
-                            }
-                        if best_model_config:
-                            best_model_config =  config if f1 > best_model_config["f1_score_test"] else best_model_config
-                        else:
-                            best_model_config = config
-                        export_confusion_matrix(confusion_m, matrix_id)
-                        save_to_json(
-                            config, f"{best_configs_dir}/{model_selected_name}-results.json")
-                        export_df_to_csv(
-                            result, f"{model_selected_name}-Results-{output_csv_idx}")
+    #                     config = {
+    #                         "outliers_method": outl_method_name,
+    #                         "feature_selector": selector_name,
+    #                         "features_selected_set": features_selected_set,
+    #                         "model": model_selected_name,
+    #                         "best-config": result["params"].values[:1][0],
+    #                         "f1_score_train": result["mean_test_score"].values[:1][0],
+    #                         "f1_score_test": f1,
+    #                         "precision_test": prec,
+    #                         "recall_test": recall,
+    #                         "accuracy_test": acc,
+    #                         "confusion_matrix_path": f"{images_dir}/{matrix_id}.png"         
+    #                         }
+    #                     if best_model_config:
+    #                         best_model_config =  config if f1 > best_model_config["f1_score_test"] else best_model_config
+    #                     else:
+    #                         best_model_config = config
+    #                     export_confusion_matrix(confusion_m, matrix_id)
+    #                     save_to_json(
+    #                         config, f"{best_configs_dir}/{model_selected_name}-results.json")
+    #                     export_df_to_csv(
+    #                         result, f"{model_selected_name}-Results-{output_csv_idx}")
 
-            output_csv_idx += 1
-    best_model_name = best_model_config["model"]
-    save_to_json(best_model_config, f"{best_configs_dir}/{best_model_name}-results.json", "best")
+    #         output_csv_idx += 1
+    # best_model_name = best_model_config["model"]
+    # save_to_json(best_model_config, f"{best_configs_dir}/{best_model_name}-results.json", "best")
 
     print(f"--------Time to complete code {(time.time() - start_time)//60} minutes and {round((time.time() - start_time)%60, 2)} seconds ----" )        
 
